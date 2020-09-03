208c22da871db66eeaf000f1159ed71e60491dde
Jay T <jaytula@gmail.com>
2020-09-03 12:42:37 -0700

    Create the API: Login Step 3 - Check if user exists

--- src/lambda/login.js
+++ src/lambda/login.js
@@ -10,6 +10,14 @@ export const handler = async event => {
 
     // 2. Get the email and password from the request body
     const { email, password } = JSON.parse(event.body);
+
+    // 3. Check to see if the user exists, if not return error (401 Unauthorized)
+    const existingUser = await users.findOne({email});
+    if(existingUser == null) {
+      errorStatusCode = 401;
+      throw new Error('Invalid password or email');
+    }
+    
   } catch (err) {
     return {
       statusCode: errorStatusCode,
