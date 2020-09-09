d6a46987d547a808face8abf830d159b57aaef47
Jay T <jaytula@gmail.com>
2020-09-09 16:58:47 -0700

    Update App component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useAuth } from "./providers/auth-provider";
+import { AuthenticatedApp } from "./AuthenticatedApp";
+import { UnauthenticatedApp } from "./UnauthenticatedApp";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const { user } = useAuth();
+
+  return user ? <AuthenticatedApp /> : <UnauthenticatedApp />;
+};
 
 export default App;
